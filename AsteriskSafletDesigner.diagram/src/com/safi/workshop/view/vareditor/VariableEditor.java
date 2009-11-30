ec967d771fb05c9ad42483afc92a5a738809d55c
steamedcotton <steamedcotton@254595da-98a2-11de-83f8-39e71e4dc75d>
2009-11-30 11:12:38 +0000

    r1098@nebiki2 (orig r2080):  zacw | 2009-11-29 15:44:48 -0800

--- AsteriskSafletDesigner.diagram/src/com/safi/workshop/view/vareditor/VariableEditor.java
+++ AsteriskSafletDesigner.diagram/src/com/safi/workshop/view/vareditor/VariableEditor.java
@@ -165,6 +165,7 @@ public class VariableEditor extends TitleAreaDialog {
     composite = new Composite(container, SWT.NONE);
     composite.setLayoutData(new GridData());
     final GridLayout gridLayout_1 = new GridLayout();
+    gridLayout_1.marginWidth = 0;
     gridLayout_1.numColumns = 2;
     composite.setLayout(gridLayout_1);
 
@@ -656,10 +657,10 @@ private void updateTypeFormat(VariableType varType) {
       this.formatValueLabel.setText("1");
       break;
     case OBJECT:
-      this.formatValueLabel.setText("Script object can be assigned.");
+      this.formatValueLabel.setText("Any object type can be assigned.");
       break;
     case TEXT:
-      this.formatValueLabel.setText("Enter without quotes");
+      this.formatValueLabel.setText("Mary had a little lamb");
       break;
     case TIME:
       //this.valueText.setFormatter(this.timeFormatter);	
