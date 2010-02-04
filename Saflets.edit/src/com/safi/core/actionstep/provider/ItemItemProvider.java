1d2f9ffaf91cabb6859db42acdc254b5f58a1d80
steamedcotton <steamedcotton@254595da-98a2-11de-83f8-39e71e4dc75d>
2010-02-04 11:19:10 +0000

    r1660@nebiki2 (orig r2657):  zacw | 2010-02-03 22:58:14 -0800

--- Saflets.edit/src/com/safi/core/actionstep/provider/ItemItemProvider.java
+++ Saflets.edit/src/com/safi/core/actionstep/provider/ItemItemProvider.java
@@ -7,19 +7,11 @@
 package com.safi.core.actionstep.provider;
 
 
-import com.safi.core.actionstep.ActionStepPackage;
-import com.safi.core.actionstep.Item;
-
-import com.safi.core.provider.CoreEditPluginProv;
-
 import java.util.Collection;
 import java.util.List;
-
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.common.util.ResourceLocator;
-
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -28,8 +20,11 @@ import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
-import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 import org.eclipse.emf.edit.provider.ViewerNotification;
+import com.safi.core.actionstep.ActionStepPackage;
+import com.safi.core.actionstep.Item;
+import com.safi.core.provider.CoreEditPluginProv;
+import com.safi.core.provider.ThreadSensitiveItemProvider;
 
 /**
  * This is the item provider adapter for a {@link com.safi.core.actionstep.Item} object.
@@ -38,7 +33,7 @@ import org.eclipse.emf.edit.provider.ViewerNotification;
  * @generated
  */
 public class ItemItemProvider
-  extends ItemProviderAdapter
+  extends ThreadSensitiveItemProvider
   implements
     IEditingDomainItemProvider,
     IStructuredItemContentProvider,
