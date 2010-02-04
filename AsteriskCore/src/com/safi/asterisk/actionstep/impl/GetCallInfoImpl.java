8d340630eb15c3ecb9b165c15cda107abe5749f5
steamedcotton <steamedcotton@254595da-98a2-11de-83f8-39e71e4dc75d>
2010-02-04 11:16:28 +0000

    r1653@nebiki2 (orig r2648):  zacw | 2010-02-03 22:30:26 -0800

--- AsteriskCore/src/com/safi/asterisk/actionstep/impl/GetCallInfoImpl.java
+++ AsteriskCore/src/com/safi/asterisk/actionstep/impl/GetCallInfoImpl.java
@@ -26,6 +26,7 @@ import com.safi.core.actionstep.impl.ActionStepImpl;
 import com.safi.core.actionstep.util.VariableTranslator;
 import com.safi.core.saflet.SafletContext;
 import com.safi.core.saflet.SafletEnvironment;
+import com.safi.core.saflet.SafletException;
 import com.safi.db.Variable;
 import com.safi.db.VariableScope;
 
@@ -202,12 +203,22 @@ public class GetCallInfoImpl extends ActionStepImpl implements GetCallInfo {
     AgiChannel channel = call1.getChannel();
     Exception exception = null;
     try {
-      AgiRequest request = (AgiRequest) context
-          .getVariableRawValue(AsteriskSafletConstants.VAR_KEY_REQUEST);
+      AgiRequest request = null;
       
-      if (accountCodeVar != null)
+      try {
+	      final Object val = context
+        .getVariableRawValue(AsteriskSafletConstants.VAR_KEY_REQUEST);
+	      if (val != null && val instanceof AgiRequest)
+				request = (AgiRequest) val;
+      } catch (Exception e) {
+	      // TODO: handle exception
+      }
+      
+      if (accountCodeVar != null) {
+      	if (request == null)
+      		throw new SafletException("No AgiRequest object available for current context");
         try {
-
+        	
           Object result = request.getAccountCode();
           setVariableValue(accountCodeVar, result, context);
 
@@ -216,6 +227,7 @@ public class GetCallInfoImpl extends ActionStepImpl implements GetCallInfo {
            debugLog.log(Level.SEVERE, e.getLocalizedMessage());
           exception = e;
         }
+      }
 
       if (callerIdNameVar != null)
         try {
@@ -250,7 +262,9 @@ public class GetCallInfoImpl extends ActionStepImpl implements GetCallInfo {
           exception = e;
         }
 
-      if (contextVar != null)
+      if (contextVar != null) {
+      	if (request == null)
+      		throw new SafletException("No AgiRequest object available for current context");
         try {
           Object result = request.getContext();
           setVariableValue(contextVar, result, context);
@@ -260,7 +274,10 @@ public class GetCallInfoImpl extends ActionStepImpl implements GetCallInfo {
           exception = e;
         }
 
-      if (extensionVar != null)
+      }
+      if (extensionVar != null) {
+      	if (request == null)
+      		throw new SafletException("No AgiRequest object available for current context");
         try {
           Object result = request.getExtension();
           setVariableValue(extensionVar, result, context);
@@ -269,9 +286,13 @@ public class GetCallInfoImpl extends ActionStepImpl implements GetCallInfo {
            debugLog.log(Level.SEVERE, e.getLocalizedMessage());
           exception = e;
         }
-
-      if (dialedNumber != null)
+      }
+      
+      if (dialedNumber != null) {
+      	if (request == null)
+      		throw new SafletException("No AgiRequest object available for current context");
         try {
+        	
           Object result = request.getDnid();
           setVariableValue(dialedNumber, result, context);
         } catch (Exception e) {
@@ -279,8 +300,11 @@ public class GetCallInfoImpl extends ActionStepImpl implements GetCallInfo {
            debugLog.log(Level.SEVERE, e.getLocalizedMessage());
           exception = e;
         }
+      }
 
-      if (priorityVar != null)
+      if (priorityVar != null) {
+      	if (request == null)
+      		throw new SafletException("No AgiRequest object available for current context");
         try {
           Object result = request.getPriority();
           setVariableValue(priorityVar, result, context);
@@ -289,7 +313,7 @@ public class GetCallInfoImpl extends ActionStepImpl implements GetCallInfo {
            debugLog.log(Level.SEVERE, e.getLocalizedMessage());
           exception = e;
         }
-
+      }
       if (stateVar != null)
         try {
           Object result = call1.getChannel().getChannelStatus();
@@ -310,7 +334,9 @@ public class GetCallInfoImpl extends ActionStepImpl implements GetCallInfo {
           exception = e;
         }
 
-      if (rdnis != null)
+      if (rdnis != null) {
+      	if (request == null)
+      		throw new SafletException("No AgiRequest object available for current context");
         try {
           Object result = request.getRdnis();
           setVariableValue(rdnis, result, context);
@@ -319,8 +345,11 @@ public class GetCallInfoImpl extends ActionStepImpl implements GetCallInfo {
            debugLog.log(Level.SEVERE, e.getLocalizedMessage());
           exception = e;
         }
+      }
 
-      if (ani2Var != null)
+      if (ani2Var != null) {
+      	if (request == null)
+      		throw new SafletException("No AgiRequest object available for current context");
         try {
           Object result = request.getCallingAni2();
           setVariableValue(ani2Var, result, context);
@@ -329,8 +358,11 @@ public class GetCallInfoImpl extends ActionStepImpl implements GetCallInfo {
            debugLog.log(Level.SEVERE, e.getLocalizedMessage());
           exception = e;
         }
-
-      if (type != null)
+      }
+      
+      if (type != null) {
+      	if (request == null)
+      		throw new SafletException("No AgiRequest object available for current context");
         try {
           Object result = request.getType();
           setVariableValue(type, result, context);
@@ -339,7 +371,7 @@ public class GetCallInfoImpl extends ActionStepImpl implements GetCallInfo {
            debugLog.log(Level.SEVERE, e.getLocalizedMessage());
           exception = e;
         }
-
+      }
     } catch (Exception e) {
       handleException(context, e);
       return;
