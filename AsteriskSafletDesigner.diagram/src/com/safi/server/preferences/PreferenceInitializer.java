665d07b63015458ce9f18036ce118fa652611d59
steamedcotton <steamedcotton@254595da-98a2-11de-83f8-39e71e4dc75d>
2010-06-15 10:20:07 +0000

    r1866@nebiki2 (orig r2930):  zacw | 2010-06-15 02:14:47 -0700  combining AsteriskSafiServer plugin

---
+++ AsteriskSafletDesigner.diagram/src/com/safi/server/preferences/PreferenceInitializer.java
@@ -0,0 +1,46 @@
+package com.safi.server.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+import com.safi.server.plugin.SafiServerPlugin;
+
+public class PreferenceInitializer extends AbstractPreferenceInitializer {
+
+  public PreferenceInitializer() {
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public void initializeDefaultPreferences() {
+    // TODO Auto-generated method stub
+
+    IPreferenceStore store = getPreferenceStore();
+    store.setDefault(PreferenceConstants.PREF_DB_PORT_PROD, 7021); //store.setDefault(PreferenceConstants.PREF_DB_PORT_PROD, 7021);
+//    store.setDefault(PreferenceConstants.AST_DESCRIPTION_DEBUG, "Local debug Asterisk server");
+//    store.setDefault(PreferenceConstants.PREF_AST_IP_DEBUG, "");
+//    store.setDefault(PreferenceConstants.PREF_MGR_USER_DEBUG, "");
+//    store.setDefault(PreferenceConstants.PREF_MGR_PWD_DEBUG, "");
+//    store.setDefault(PreferenceConstants.PREF_MANAGER_PORT_DEBUG, 5038); //    store.setDefault(PreferenceConstants.PREF_MANAGER_PORT_DEBUG, 5038); 
+//    store.setDefault(PreferenceConstants.AST_NAME_DEBUG, "Local"+SafiServerPlugin.AST_DEBUG_NAME_SUFFIX);
+//    store.setDefault(PreferenceConstants.AST_SFTP_USER_DEBUG, "");
+//    store.setDefault(PreferenceConstants.AST_SFTP_PASSWORD_DEBUG, "");
+//    store.setDefault(PreferenceConstants.AST_SFTP_PORT_DEBUG, -1);
+//    store.setDefault(PreferenceConstants.AST_ACTIVE_DEBUG, false);
+//    store.setDefault(PreferenceConstants.PREF_AST_ID_DEBUG, -1);
+//    store.setDefault(PreferenceConstants.AST_PRIVATE_DEBUG, true);
+//    store.setDefault(PreferenceConstants.PREF_FASTAGI_PORT_DEBUG, 3573);//store.setDefault(PreferenceConstants.PREF_FASTAGI_PORT_DEBUG, 4573);
+    store.setDefault(PreferenceConstants.PREF_SERVER_ERROR_NOTIFICATION, true);
+    store.setDefault(PreferenceConstants.PREF_SERVER_INFO_NOTIFICATION, true);
+    store.setDefault(PreferenceConstants.PREF_SERVER_TRACELOG_LEVEL, PreferenceConstants.PREF_LOG_LEVEL_DEBUG);
+//    store.setDefault(PreferenceConstants.PREF_SERVER_MGT_PORT_DEBUG, 6020); // store.setDefault(PreferenceConstants.PREF_SERVER_MGT_PORT_DEBUG, 7020);
+//    store.setDefault(PreferenceConstants.PREF_SAFI_BIND_ADDR_LOCAL, "0.0.0.0");
+    store.setDefault(PreferenceConstants.PREF_SERVER_INFO_UPDATE_PERIOD, 30);
+    store.setDefault(PreferenceConstants.PREF_SSH_SERVER_PORT, 8822);
+//    store.setDefault(PreferenceConstants.PREF_PORT_FORWARDING_ENABLED, true);
+  }
+
+  protected IPreferenceStore getPreferenceStore() {
+    return SafiServerPlugin.getDefault().getPreferenceStore();
+  }
+}
---
+++ AsteriskSafletDesigner.diagram/src/com/safi/server/preferences/WorkshopPreferencePage.java
@@ -0,0 +1,133 @@
+package com.safi.server.preferences;
+
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.RadioGroupFieldEditor;
+import org.eclipse.jface.preference.ScaleFieldEditor;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Scale;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+import com.safi.server.manager.SafiServerRemoteManager;
+import com.safi.server.plugin.SafiServerPlugin;
+
+public class WorkshopPreferencePage extends FieldEditorPreferencePage implements
+    IWorkbenchPreferencePage {
+
+  private Scale scale;
+  private DataBindingContext m_bindingContext;
+  private Label label;
+  private ScaleFieldEditor scaleFieldEditor;
+
+  /**
+   * Create the preference page
+   */
+  public WorkshopPreferencePage() {
+    super(FieldEditorPreferencePage.GRID);
+    setPreferenceStore(SafiServerPlugin.getDefault().getPreferenceStore());
+  }
+
+  /**
+   * Create contents of the preference page
+   */
+  @Override
+  protected void createFieldEditors() {
+    {
+      final BooleanFieldEditor serverErrorNotificationField = new BooleanFieldEditor(
+          PreferenceConstants.PREF_SERVER_ERROR_NOTIFICATION,
+          "Show SafiServer error notifications", getFieldEditorParent());
+      addField(serverErrorNotificationField);
+    }
+
+    {
+      final BooleanFieldEditor serverInfoNotificationField = new BooleanFieldEditor(
+          PreferenceConstants.PREF_SERVER_INFO_NOTIFICATION, "Show SafiServer info notifications",
+          getFieldEditorParent());
+      addField(serverInfoNotificationField);
+    }
+
+    {
+      addField(new RadioGroupFieldEditor(PreferenceConstants.PREF_SERVER_TRACELOG_LEVEL,
+          "Server Log Level", 1, new String[][] {
+              new String[] { "None", PreferenceConstants.PREF_LOG_LEVEL_OFF },
+              new String[] { "Debug", PreferenceConstants.PREF_LOG_LEVEL_DEBUG },
+              new String[] { "Info", PreferenceConstants.PREF_LOG_LEVEL_INFO },
+              new String[] { "Warn", PreferenceConstants.PREF_LOG_LEVEL_WARN },
+              new String[] { "Error", PreferenceConstants.PREF_LOG_LEVEL_ERROR } },
+          getFieldEditorParent()));
+    }
+
+    {
+      Composite fieldEditorParent = getFieldEditorParent();
+      scaleFieldEditor = new ScaleFieldEditor(PreferenceConstants.PREF_SERVER_INFO_UPDATE_PERIOD,
+          "", fieldEditorParent);
+      label = scaleFieldEditor.getLabelControl(fieldEditorParent);
+      scale = scaleFieldEditor.getScaleControl();
+      scale.setToolTipText("Frequency with which SafiServer status page will be updated");
+      scale.addSelectionListener(new SelectionAdapter() {
+        public void widgetSelected(final SelectionEvent e) {
+          updateInfoUpdateFreqLabel();
+        }
+      });
+      scaleFieldEditor.setPageIncrement(50);
+      scaleFieldEditor.setMaximum(300);
+
+      scaleFieldEditor.setLabelText(String
+          .format("SafiServer info update freq (%10s)", String.valueOf(getPreferenceStore().getInt(
+              PreferenceConstants.PREF_SERVER_INFO_UPDATE_PERIOD))));
+      addField(scaleFieldEditor);
+      if (!SafiServerPlugin.getDefault().isConnected())
+        scaleFieldEditor.setEnabled(false, scaleFieldEditor.getScaleControl().getParent());
+
+    }
+    // Create the field editors
+    // updateInfoUpdateFreqLabel();
+  }
+
+  /**
+   * Initialize the preference page
+   */
+  public void init(IWorkbench workbench) {
+    // Initialize the preference page
+    updateInfoUpdateFreqLabel();
+  }
+
+  @Override
+  public boolean performOk() {
+    SafiServerRemoteManager.getInstance().notificationPreferencesChanged();
+    return super.performOk();
+  }
+
+  // protected DataBindingContext initDataBindings() {
+  // IObservableValue labelTextObserveWidget = SWTObservables.observeText(label);
+  // IObservableValue scaleSelectionObserveWidget =
+  // SWTObservables.observeSelection(scale);
+  // //
+  // //
+  // DataBindingContext bindingContext = new DataBindingContext();
+  // //
+  // bindingContext.bindValue(scaleSelectionObserveWidget, labelTextObserveWidget, null,
+  // null);
+  // //
+  // return bindingContext;
+  // }
+
+  private void updateInfoUpdateFreqLabel() {
+    if (scale == null || !SafiServerPlugin.getDefault().isConnected())
+      return;
+    int val = scale.getSelection();
+    String valStr;
+    if (val == scale.getMinimum())
+      valStr = "NEVER";
+    else
+      valStr = String.valueOf(val) + " secs";
+    scaleFieldEditor.setLabelText(String.format("SafiServer info update freq (%10s)", valStr));
+    // scaleFieldEditor.getScaleControl().pack();
+  }
+
+}
